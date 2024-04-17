3bf1931be5439e73c43e28491a3d29fb19e55d79
prathibha <prathibha@Prathibha>
2024-04-17 20:36:09 +0530

    create book functionality added

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const app = express();
+const { Book, User } = require("./models");
+const path = require("path");
 
-// Define your routes
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.get('/', async (req, res) => {
+  try {
+    const books = await Book.findAll();
+    res.render('Home', { books }); // Pass books data to the Home view
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/newBook', (req, res) => {
+  res.render('newBook'); 
+});
+
+app.post('/books', async (req, res) => {
+  try {
+    const { title, author, price } = req.body;
+    const book = await Book.create({ title, author, price });
+    res.redirect('/'); // Redirect to the home page after adding a new book
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = app;
--- models/book.js
+++ models/book.js
@@ -1,18 +1,22 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Book extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
+
+    static async addBook(title, author, price) {
+      try {
+        const book = await Book.create({ title, author, price });
+        return book;
+      } catch (error) {
+        throw new Error('Failed to add book');
+      }
+    }
   }
+
   Book.init({
     title: DataTypes.STRING,
     author: DataTypes.STRING,
@@ -21,5 +25,6 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'Book',
   });
+
   return Book;
 };
